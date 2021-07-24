25c8823bdf417a1e51a9effce25f25398fcf9f93
Kumar Aishwary <kumar.aishwary@tigeranalytics.local>
2021-07-24 21:01:09 +0530

    fixed label bug

--- dataloaders/imaterialist.py
+++ dataloaders/imaterialist.py
@@ -15,7 +15,7 @@ class Imaterialist(BaseDataSet):
         self.DATA_DIR = os.path.join("data","imaterialist")
         with open(os.path.join(self.DATA_DIR, "label_descriptions.json")) as f:
             self.label_descriptions = json.load(f)
-        
+
         segment_df = pd.read_csv(os.path.join(self.DATA_DIR,"train.csv"))
         label_names = [x['name'] for x in self.label_descriptions['categories']]
         
@@ -23,17 +23,16 @@ class Imaterialist(BaseDataSet):
         self.warp_image = warp_image
         self.num_classes = len(label_names)
         self.palette = palette.get_voc_palette(self.num_classes)
-        self.file_index = list(self.image_df.index)
         super(Imaterialist, self).__init__(**kwargs)
 
     def _set_files(self):
         if self.split in ['train', 'test']:
             file_path = os.path.join(self.DATA_DIR, self.split, "*")
-            self.files = [os.path.basename(x) for x in glob(file_path)]
+            self.files = list(self.image_df.index)
         else: raise ValueError(f"Invalid split name {self.split} choose one of [train, test]")
 
     def _load_data(self, index):
-        image_id = self.file_index[index]
+        image_id = self.files[index]
         image_path = os.path.join(self.DATA_DIR, self.split, image_id + ".jpg")
         image = np.asarray(Image.open(image_path).convert('RGB'), dtype=np.float32)
         
@@ -56,7 +55,8 @@ class Imaterialist(BaseDataSet):
         class_ids = self.image_df.loc[f, "ClassId"]
         height, width = shape[:2]
         
-        mask = np.zeros((height, width)).reshape(-1)
+        mask = np.zeros((height, width), dtype = np.uint8).reshape(-1)
+        mask -= 1
         for segment, (pixel_str, class_id) in enumerate(zip(encoded_pixels, class_ids)):
             splitted_pixels = list(map(int, pixel_str.split()))
             pixel_starts = splitted_pixels[::2]
@@ -65,7 +65,7 @@ class Imaterialist(BaseDataSet):
             for pixel_start, run_length in zip(pixel_starts, run_lengths):
                 pixel_start = int(pixel_start) - 1
                 run_length = int(run_length)
-                mask[pixel_start:pixel_start+run_length] = 255 - class_id * 4
+                mask[pixel_start:pixel_start+run_length] = class_id
         mask = mask.reshape([height, width], order='F')
         return mask
         
