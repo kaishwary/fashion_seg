d8855d192b54823c23855a58e726d2d9a2cf1527
Kumar Aishwary <kaishwary@gmail.com>
2021-07-25 19:12:06 +0530

    added image->hdfs modification scripts

---
+++ convert_data.sh
@@ -0,0 +1,7 @@
+python convert_to_hdfs.py 0 5000 &
+python convert_to_hdfs.py 5000 10000 &
+python convert_to_hdfs.py 10000 15000 &
+python convert_to_hdfs.py 15000 20000 &
+python convert_to_hdfs.py 20000 25000 &
+python convert_to_hdfs.py 25000 30000 &
+python convert_to_hdfs.py 30000 45623 &
--- convert_to_hdfs.py
+++ convert_to_hdfs.py
@@ -6,11 +6,12 @@ import time
 import os
 import numpy as np
 from pathlib import Path
-
+from PIL import Image
 #%% Create path if not exists
 
 Path(os.path.join("data", "imat_reduced", "train")).mkdir(parents=True, exist_ok=True)
 Path(os.path.join("data", "imat_reduced", "test")).mkdir(parents=True, exist_ok=True)
+Path(os.path.join("data", "imat_reduced", "new")).mkdir(parents=True, exist_ok=True)
 #%%
 
 def store_single_hdf5(image, label, image_id):
@@ -39,33 +40,34 @@ def convert_to_hdfs(image, mask, image_id):
 def converter_func(start_idx, end_idx, loader):
     print("Converting from", start_idx, "to", end_idx)
     start = time.time()
-    for i in range(20):
+    for i in range(start_idx, end_idx):
         image, mask, image_id = loader.dataset._load_data(i)
         convert_to_hdfs(image, mask, image_id)
     end = time.time()
+    print("Time taken", end-start)
     
+def converter_jpeg(loader):
+#    print("Converting from", start_idx, "to", end_idx)
+    start = time.time()
+    for i in range(20):
+        image, mask, image_id = loader.dataset._load_data(i)
+        np.save(arr = image,file = os.path.join("data", "imat_reduced", "new", image_id))
+    end = time.time()
+
     print("Time taken", end-start)
 
+#%%   
 if __name__ == "__main__":
-    import multiprocessing
     
     start = time.time()
     loader = IMAT(data_dir="af", num_workers=0, batch_size=10, split="train")
     
-    p1 = multiprocessing.Process(target=converter_func, args=(0,100,loader,))
-    p2 = multiprocessing.Process(target=converter_func, args=(100,200,loader,))
-    p3 = multiprocessing.Process(target=converter_func, args=(300,400,loader,))
-    p4 = multiprocessing.Process(target=converter_func, args=(500,600,loader,))
+    import sys
+
+    start_idx = int(sys.argv[1])
+    end_idx = int(sys.argv[2])
     
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
+    converter_func(start_idx, end_idx, loader)
     
     end = time.time()
     print("Total time taken:", end-start)
